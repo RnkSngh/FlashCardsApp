47c7b8b003a07e8afb360f59932971ce14cc277b
Raunak <60561127+RnkSngh@users.noreply.github.com>
2021-03-12 07:42:03 -0800

    Update settings.py

--- FlashCardProject/FlashCardProject/settings.py
+++ FlashCardProject/FlashCardProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a*0&22=zlns+i02&p_3*0qrqk9q9lq#6vit6nu(a4jkq1_lhsw'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
